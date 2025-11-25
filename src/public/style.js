ae6058e8e5926addfce1b40ff12e9a9b87d945df
Isabel Diniz <bebeldiniz2017@gmail.com>
2025-11-25 12:25:02 -0300

    animação e ajustes na tela inicial

---
+++ src/public/style.js
@@ -0,0 +1,54 @@
+const header = document.getElementsByTagName('header');
+const anchor = document.getElementsByTagName('a');
+const h1 = document.getElementsByTagName('h1');
+const h2 = document.getElementsByTagName('h2');
+const h3 = document.getElementsByTagName('h3');
+const p = document.getElementsByTagName('p');
+const body = document.getElementsByTagName('body');
+const centerTitle = document.getElementById('background');
+
+let ultimaPosicaoScroll = window.scrollY;
+
+const checkBox = document.getElementById('changeTheme');
+
+    window.addEventListener('scroll', ()=>{
+        const posicaoAtualScrool = window.scrollY;
+
+        if (posicaoAtualScrool > ultimaPosicaoScroll){
+            console.log('Deslizou para baixo');
+            centerTitle.style.height = '100px';
+            centerTitle.style.top = '100px';
+        } else if (posicaoAtualScrool < ultimaPosicaoScroll){
+            console.log('Deslizou para cima');
+        }
+
+        ultimaPosicaoScroll = posicaoAtualScrool
+    })
+
+    checkBox.addEventListener('change', ()=>{//função para trocar o tema do site 
+        if(checkBox.checked){
+            console.log('clicado');
+            Array.from(header).forEach(header => {//array.from server para transformar o header em um array, pois ele retorna um HTML collection, assim devemos acessar cada elemento dessa coleção usando o forEach, que é uma função exclusiva de arrays
+                header.style.backgroundColor = 'black';
+            });
+            Array.from(anchor).forEach(a => {
+                a.style.color = 'white';
+            });
+            Array.from(body).forEach(body => {
+                body.style.backgroundColor = 'black';
+                body.style.color = 'white';
+            });
+        } else {
+            console.log('clicado de novo');
+            Array.from(header).forEach(header => {
+                header.style.backgroundColor = 'white';
+            });
+            Array.from(anchor).forEach(header => {
+                header.style.color = 'black';
+            });
+            Array.from(body).forEach(body => {
+                body.style.backgroundColor = 'white';
+                body.style.color = 'black';
+            });
+        }
+    });
